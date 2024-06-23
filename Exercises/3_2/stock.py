8a97d75be0b9a3bd599b378d21583e14d0e4155c
Harshad Bhusare <harshadbhusare022@gmail.com>
2024-06-23 20:00:09 +0530

    23-06

---
+++ Exercises/3_2/stock.py
@@ -0,0 +1,8 @@
+class Stock():
+    def __init__(self, name, stocks, price):
+        self.name = str(name)
+        self.stocks = int(stocks)
+        self.price = float(price)
+        
+
+        
